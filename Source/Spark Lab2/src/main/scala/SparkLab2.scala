5527f9742813a04bbbbb4d30af6f9183632dbdc0
Jackie <jbvx8@mail.umkc.edu>
2017-02-01 22:19:50 -0600

    delete outputs and add comments

--- Source/Spark Lab2/src/main/scala/SparkLab2.scala
+++ Source/Spark Lab2/src/main/scala/SparkLab2.scala
@@ -16,12 +16,16 @@ object SparkLab2 {
 
     val input=sc.textFile("input")
 
+    // create list of lowercase characters from input file
     val letters = input.flatMap(line => {line.toLowerCase.toList})
 
+    // filter out any punctuation characters from the list
     val noPunctuation = letters.filter(letter => {!("" + letter).replaceAll("\\p{P}", "").equals("")})
 
+    // create map of characters
     val noPunctuationCharacters = noPunctuation.map(letter=>(letter,1)).cache()
 
+    // combine same characters with total count of each
     val output=noPunctuationCharacters.reduceByKey(_+_)
 
     output.saveAsTextFile("output")
@@ -35,6 +39,7 @@ object SparkLab2 {
 
     }}
 
+    // combine output files into one file
   output.repartition(1).saveAsTextFile("combine");
 
   }
